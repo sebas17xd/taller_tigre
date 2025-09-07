5ec0d7e107537bb581a234cab4e9d7234995b77b
sebas17xd <sebastianchaves30@gmail.com>
2025-09-07 16:48:07 -0500

    Agrego js y modo oscuro

---
+++ script.js
@@ -0,0 +1,9 @@
+// Cambiar tema claro/oscuro
+function cambiarTema() {
+  document.body.classList.toggle("oscuro");
+}
+
+// Mostrar información adicional
+function mostrarMas() {
+  alert("Los tigres son excelentes nadadores y pueden recorrer largas distancias en el agua.");
+}
